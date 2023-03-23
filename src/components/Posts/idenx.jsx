ab396f5f62dac3b0f0014933ee832e0cd9d80f76
Osvaldo Sousa <osvaldo.msn18@gmail.com>
2023-03-23 15:51:00 -0300

    create component Post and Css
    fixed erro commit

---
+++ src/components/Posts/idenx.jsx
@@ -0,0 +1,28 @@
+import styles from './Posts.module.css'
+
+function Posts({ posts }) {
+  console.log(posts)
+
+  return (
+    <>
+      {posts.map(post => {
+        return (
+          <li key={post.id}>
+            <div className={styles.post}>
+              <img
+                className={styles.banner}
+                src={`../../../public/posts/${post.id}/capa.png`}
+                alt={`Capa do post`}
+              />
+
+              <h2 className={styles.title}>{post.titulo}</h2>
+              <button className={styles.buttonRead}>Ler</button>
+            </div>
+          </li>
+        )
+      })}
+    </>
+  )
+}
+
+export default Posts
